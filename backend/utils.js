a692a193ad9e64889f25d9dd3a5c67159372cedd
farizsalim <farizsalim10@yahoo.co.id>
2023-01-14 09:56:53 +0800

    Selfhealing-v0.22

--- backend/utils.js
+++
@@ -1,5 +0,0 @@
-const moment = require('moment')
-
-const date = () => moment(new Date()).format("YYYY-MM-D H:mm:ss");
-
-module.exports = {date};
--- frontend/src/routes.js
+++
@@ -1,7 +0,0 @@
-const HOME = "/";
-const LOGIN = "/loginpage"
-const SIGNUP = "/signuppage"
-const TROOM = "/therapyroompage"
-const JENK = "/jenjangkarir"
-
-export {HOME,LOGIN,SIGNUP,TROOM,JENK};
