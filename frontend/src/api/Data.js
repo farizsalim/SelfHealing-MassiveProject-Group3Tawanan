5689ebe419b382ffb2dcf2f44e57433c4b4fd8ba
Andi Nurdin <20082010059@student.upnjatim.ac.id>
2023-01-20 10:22:46 +0700

    update page therapy room

--- frontend/src/api/Data.js
+++ frontend/src/api/Data.js
@@ -5,27 +5,27 @@ export const Medroom = [
         text: "7-10 Menit"
     },
     {
-        img: require("../images/therapy1.png"),
+        img: require("../images/terapi2.png"),
         title: "Restful Sleep at night",
         text: "7-10 Menit"
     },
     {
-        img: require("../images/therapy1.png"),
+        img: require("../images/terapi3.png"),
         title: "Restful Sleep at night",
         text: "7-10 Menit"
     },
     {
-        img: require("../images/therapy1.png"),
+        img: require("../images/terapi4.png"),
         title: "Restful Sleep at night",
         text: "7-10 Menit"
     },
     {
-        img: require("../images/therapy1.png"),
+        img: require("../images/terapi5.png"),
         title: "Restful Sleep at night",
         text: "7-10 Menit"
     },
     {
-        img: require("../images/therapy1.png"),
+        img: require("../images/terapi6.png"),
         title: "Restful Sleep at night",
         text: "7-10 Menit"
     },
