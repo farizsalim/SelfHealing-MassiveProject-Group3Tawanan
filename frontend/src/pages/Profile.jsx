8a2e01e548556e00c386d80922e8d5a0397150ad
Andi Nurdin <20082010059@student.upnjatim.ac.id>
2023-01-20 22:45:08 +0700

    update page profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,4 +1,4 @@
-import { Container } from "react-bootstrap";
+import { Container, Card } from "react-bootstrap";
 import Footer from "../components/Footer"
 import "./Profile.css";
 
@@ -10,6 +10,16 @@ const Profile = () => {
           <h1 className="title-aktivitas">Aktivitas</h1>
           <div className="line-aktivitas"></div>
         </div>
+        <Card style={{ borderStyle: "none" }}>
+          <Card.Body className="card-aktivity">
+            <Card.Title>
+              <h1 className="hcard-aktivity">Deskripsikan mood kamu hari ini</h1>
+            </Card.Title>
+            <Card.Text className="tcard-aktivity">
+              <h3>mood kamu hari ini...</h3>
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </Container>
       <Footer />
     </section>
