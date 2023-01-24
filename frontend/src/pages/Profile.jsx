7cabf6db543b1f35705bd8acfc0f0fc8ce6973f9
farizsalim <farizsalim10@yahoo.co.id>
2023-01-24 07:09:46 -0800

    Repair Profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -20,6 +20,16 @@ const Profile = () => {
           <h1 className="title-aktivitas">Aktivitas</h1>
           <div className="line-aktivitas"></div>
         </div>
+        <Card style={{ borderStyle: "none" }}>
+          <Card.Body className="card-aktivity">
+            <Card.Title>
+              <h1 className="hcard-aktivity">Deskripsikan mood kamu hari ini</h1>
+            </Card.Title>
+            <Card.Text className="tcard-aktivity">
+              <h3>mood kamu hari ini...</h3>
+            </Card.Text>
+          </Card.Body>
+        </Card>
       </Container>
       )}
       <Footer />
